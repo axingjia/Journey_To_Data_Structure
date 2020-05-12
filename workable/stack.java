a7b4692817ec1e34f81052d9867bb4bfadb1064c
Alan Huang <axingjia@gmail.com>
2020-05-12 15:16:58 -0700

    fixing stack

--- workable/stack.java
+++ workable/stack.java
@@ -3,52 +3,56 @@
 // to run this program: C>java StackApp ////////////////////////////////////////////////////////////////
 class StackX
 {
-private int maxSize; // size of stack array private
-long[] stackArray;
-private int top; // top of stack
-//--------------------------------------------------------------
-public StackX(int s) // constructor
-{
-maxSize = s; // set array size
-stackArray = new long[maxSize]; // create array
-top = -1; // no items yet
-}
-//--------------------------------------------------------------
-public void push(long j) // put item on top of stack
-{
-stackArray[++top] = j; // increment top, insert item
-}
+    private int maxSize; // size of stack array private
+    long[] stackArray;
+    private int top; // top of stack
+    //--------------------------------------------------------------
+    public StackX(int s) // constructor
+    {
+        maxSize = s; // set array size
+        stackArray = new long[maxSize]; // create array
+        top = -1; // no items yet
+    }
+    //--------------------------------------------------------------
+    public void push(long j) // put item on top of stack
+    {
+        stackArray[++top] = j; // increment top, insert item
+    }
 
-//--------------------------------------------------------------
-public long pop() // take item from top of stack
-{
-return stackArray[top--]; // access item, decrement top
-}
-//--------------------------------------------------------------
-public long peek() // peek at top of stack
-{
-return stackArray[top]; }
-//--------------------------------------------------------------
-public boolean isEmpty() // true if stack is empty
-{
-return (top == -1); }
-//--------------------------------------------------------------
-public boolean isFull() // true if stack is full
-{
-return (top == maxSize-1); }
+    //--------------------------------------------------------------
+    public long pop() // take item from top of stack
+    {
+        return stackArray[top--]; // access item, decrement top
+    }
+    //--------------------------------------------------------------
+        public long peek() // peek at top of stack
+    {
+        return stackArray[top];
+    }
+    //--------------------------------------------------------------
+    public boolean isEmpty() // true if stack is empty
+    {
+        return (top == -1);
+    }
+    //--------------------------------------------------------------
+    public boolean isFull() // true if stack is full
+    {
+        return (top == maxSize-1);
+    }
 //--------------------------------------------------------------
 } // end class StackX
 ////////////////////////////////////////////////////////////////
 class StackApp
 {
-public static void main(String[] args)
-{
-StackX theStack = new StackX(10); // make new stack
-theStack.push(20); theStack.push(40); theStack.push(60); theStack.push(80);
-while( !theStack.isEmpty() ) {
-long value = theStack.pop(); System.out.print(value); System.out.print(" ");
-} // end while
-System.out.println(""); } // end main()
+    public static void main(String[] args)
+    {
+        StackX theStack = new StackX(10); // make new stack
+        theStack.push(20); theStack.push(40); theStack.push(60); theStack.push(80);
+        while( !theStack.isEmpty() ) {
+            long value = theStack.pop(); System.out.print(value); System.out.print(" ");
+        } // end while
+        System.out.println("");
+    } // end main()
 
 } // end class StackApp
 ////////////////////////////////////////////////////////////////
